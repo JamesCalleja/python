8c0cce13b26cd41de796fb83a6de64286408e4c0
jcalsp <james.calleja@spotlight.com>
2021-11-09 16:51:00 -0500

    extending if

--- fucktions/if_statements.py
+++ fucktions/if_statements.py
@@ -12,3 +12,14 @@ elif is_tall and not(is_male):
     print("you are a female")
 else:
     print("this is the else")
+
+def max_mum (num1, num2,num3):
+    if num1 >= num2 and num1 >= mun3: # == != <=
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+print(max_mum(24,645453,325))
+
