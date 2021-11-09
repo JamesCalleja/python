c620f183f4d7608c8733729846ef0edff3b07695
jcalsp <james.calleja@spotlight.com>
2021-11-09 18:28:12 -0500

    comment

--- tuples/tuples.py
+++ tuples/tuples.py
@@ -1,5 +1,5 @@
 coordinates = (4,5)
 print(coordinates[0])
-coordinates_list = [(4,5),(98,21),(69,420)]
+coordinates_list = [(4,5),(98,21),(69,420)] # a list of tuples
 print(coordinates_list[-1])
 
