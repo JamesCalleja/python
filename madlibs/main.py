2d412830db2fbb60c83b1fa079ce06a8066094b6
jcalsp <james.calleja@spotlight.com>
2021-11-11 12:23:44 -0500

    extending madlibs

--- madlibs/main.py
+++ madlibs/main.py
@@ -5,3 +5,16 @@ celeberity = input("Enter a celeberity:")
 print("roses are " + colour)
 print(plural_noun + " are blue")
 print("I love " + celeberity)
+
+
+
+adj = input("Adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("favous person: ")
+
+madlib = f"Computer programming is so {adj}! \
+it makes me excited all the time because I love to {verb1}. \
+Stay hydrated and {verb2} like you are {famous_person}"
+
+print(madlib)
